63f64e9c4a045b8bcb64583f6e5c6ab70d1d9167
uddeepk <uddeepk@gmail.com>
2021-04-11 18:21:06 -0800

    used <br v/> for new lines

---
+++ chapter1/myhistory.php
@@ -0,0 +1,19 @@
+<?php
+  $name = "Uddeep Karki";
+  $education_history = array (
+		     "BSc CS and Math, UAF expected graduation Dec 2023",
+		     "81 credits, Southeastern Louisiana University"
+		     );
+  $work_history = array (
+  		"Assistant Web Developer, UAF eCampus, Feb 2021 - Present",
+		"Infantry, US Army, May 2016 - Dec 2020",
+		"Event Staff Student Worker, Southeastern Louisiana University, Aug 2015- Jan 2016",
+		"Student Parking Enforcer, Southeastern Louisiana University, October 2013- May 2015"
+		);
+  foreach($work_history as $w)
+  {
+	print "$w<br />";
+//	print "\n";
+  }
+
+?>
