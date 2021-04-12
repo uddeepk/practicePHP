46226de386926b45c46c263203ed43732f76a112
uddeepk <uddeepk@gmail.com>
2021-04-11 18:32:45 -0800

    used h tags

--- chapter1/myhistory.php
+++ chapter1/myhistory.php
@@ -2,7 +2,7 @@
   $name = "Uddeep Karki";
   $education_history = array (
 		     "BSc CS and Math, UAF expected graduation Dec 2023",
-		     "81 credits, Southeastern Louisiana University"
+		     "81 credits, Southeastern Louisiana University, Aug 2013 - Dec 2015"
 		     );
   $work_history = array (
   		"Assistant Web Developer, UAF eCampus, Feb 2021 - Present",
@@ -10,10 +10,16 @@
 		"Event Staff Student Worker, Southeastern Louisiana University, Aug 2015- Jan 2016",
 		"Student Parking Enforcer, Southeastern Louisiana University, October 2013- May 2015"
 		);
+  print "<h2>$name</h2>";
+  print "<h3>Work History</h3>";
   foreach($work_history as $w)
   {
 	print "$w<br />";
-//	print "\n";
   }
-
+  print "<h3>Education History</h3>";
+  foreach($education_history as $ed)
+  {
+	print "$ed<br />";
+  }
+	
 ?>
